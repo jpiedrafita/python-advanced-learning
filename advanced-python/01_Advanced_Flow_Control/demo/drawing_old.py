858f82a31e390d59e018310967f714b765cb4ec2
Jorge <4170476+jpiedrafita@users.noreply.github.com>
2025-09-01 16:21:54 +0200

    Added: Advanced Python

---
+++ advanced-python/01_Advanced_Flow_Control/demo/drawing_old.py
@@ -0,0 +1,54 @@
+class Shape:
+
+    def __init__(self, solid):
+        self.solid = solid
+
+
+class Circle(Shape):
+
+    def __init__(self, center, radius, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.center = center
+        self.radius = radius
+
+    def draw(self):
+        print("\u25cf" if self.solid else "\u25a1")
+
+
+class Parallelogram(Shape):
+
+    def __init__(self, pa, pb, pc, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pa = pa
+        self.pb = pb
+        self.pc = pc
+
+    def draw(self):
+        print("\u25b0" if self.solid else "\u25b1")
+
+
+class Triangle(Shape):
+
+    def __init__(self, pa, pb, pc, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pa = pa
+        self.pb = pb
+        self.pc = pc
+
+    def draw(self):
+        print("\u25b2" if self.solid else "\u25b3")
+
+
+def main():
+    shapes = [
+        Circle(center=(0, 0), radius=5, solid=False),
+        Parallelogram(pa=(0, 0), pb=(2, 0), pc=(1, 1), solid=False),
+        Triangle(pa=(0, 0), pb=(1, 2), pc=(2, 0), solid=True),
+    ]
+
+    for shape in shapes:
+        shape.draw()
+
+
+if __name__ == "__main__":
+    main()
